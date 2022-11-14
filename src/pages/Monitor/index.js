5b821ce410e11d4e5b9cb17c1e19d923b274c9e9
Dang Vinh <chubuc2411@gmail.com>
2022-11-14 22:34:27 +0700

    update all header page title

--- src/pages/Monitor/index.js
+++ src/pages/Monitor/index.js
@@ -1,5 +1,19 @@
+import classNames from 'classnames/bind';
+
+import styles from './Monitor.module.scss';
+import lineBelowTitle from '../../assets/images/normal_line_fpt_color.png';
+
+const sx = classNames.bind(styles);
+
 function Monitor() {
-    return ( <h1>Bảng điều khiển</h1> );
+    return (
+        <div className={sx('wrapper')}>
+            <div className={sx('page-title-container')}>
+                <h2>Bảng điều khiển</h2>
+                <img src={lineBelowTitle} alt='' />
+            </div>
+        </div>
+    );
 }
 
 export default Monitor;
--- src/pages/UserPermission/index.js
+++ src/pages/UserPermission/index.js
@@ -1,5 +1,19 @@
+import classNames from 'classnames/bind';
+
+import styles from './UserPermission.module.scss';
+import lineBelowTitle from '../../assets/images/normal_line_fpt_color.png';
+
+const sx = classNames.bind(styles)
+
 function UserPermission() {
-    return <h1>Quản lý phân quyền</h1>;
+    return (
+        <div className={sx('wrapper')}>
+            <div className={sx('page-title-container')}>
+                <h2>Quản trị phân quyền</h2>
+                <img src={lineBelowTitle} alt='' />
+            </div>
+        </div>
+    );
 }
 
 export default UserPermission;
